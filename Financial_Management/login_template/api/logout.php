a3f12aee61a64e29629ee73cee919b7a88b345e9
vidadsharon270-beep <vidadsharon270@gmail.com>
2026-07-07 15:06:03 +0800

    Delete Financial_Management directory

--- Financial_Management/login_template/api/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/functions.php';
-
-header('Content-Type: application/json');
-
-$_SESSION = [];
-session_destroy();
-
-jsonResponse(true, 'Logged out.', ['redirect' => 'login.html']);
